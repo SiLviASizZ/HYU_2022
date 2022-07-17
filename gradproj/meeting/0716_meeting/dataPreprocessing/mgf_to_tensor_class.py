d55a522d30f57557f408b105759964c90e703bad
2017029889 <kor0108silvia@gmail.com>
2022-07-18 00:08:38 +0900

    asdf

--- gradproj/meeting/0716_meeting/dataPreprocessing/mgf_to_tensor_class.py
+++ gradproj/meeting/0716_meeting/dataPreprocessing/mgf_to_tensor_class.py
@@ -11,10 +11,16 @@ class MgfToTensor():
 
     def call(self, file_path):
         f = open(file_path, 'r')
+        self.num_batch = 0
+        self.max_length = 0
+        self.return_matrix = []
+        self.return_tensor = tf.zeros(0)
+        # max_value = 0
+
         while True:
             batch_matrix = []
             PEPMASS = 0
-            CHARGE = 0
+            CHARGE = 0 
             SEQ = ''
 
             line = f.readline().replace("\n","\t")
@@ -43,20 +49,21 @@ class MgfToTensor():
                         if first == 'PEPMASS':
                             PEPMASS = float(second.strip())
                         elif first == 'CHARGE':
-                            CHARGE = float(second.strip('+-'))
+                            CHARGE = float(second.strip('+-')) # is CHARGE always positive ?
                         elif first == 'SEQ':
                             SEQ = (second.strip().replace("C(+57.02)","C").replace("M(+15.99)","m").replace("N(+.98)","n")
                                         .replace("Q(+.98)","q").replace("S(+79.97)","s")
                                         .replace("T(+79.97","t").replace("Y(+79.97)","y"))
-                            print(SEQ)
                         elif (first == 'TITLE') or (first == 'SCANS') or (first == 'RTINSECONDS'):
                             continue
                         else: #peak
+                            if max_value < second:
+                                max_value = second
                             batch_matrix.append(self.get_batch(first, second))
         f.close()
         # tf.convert_to_tensor(self.return_matrix, dtype=tf.float32)
         self.return_tensor = tf.ragged.constant(self.return_matrix).to_tensor(default_value=0)
-        return self.return_tensor
+        return self.return_tensor # (batch_num, max_length, 3)
 
     def get_batch(self, first, second):
         x = int(float(first))
@@ -81,3 +88,6 @@ pprint.pprint(k)
 transform.check_length()
 transform.check_size()
 print(tf.shape(k))
+
+# 1. charge : is it always positive ?
+# 2. intensity output?
